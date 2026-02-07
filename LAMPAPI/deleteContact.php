2a747f5c6b4b9cf2d2002e8022e182f37bfcef1d
Ricardo Metral <ricardometral2005@gmail.com>
2026-02-07 11:59:14 -0500

    Finished add, edit and delete api calls

---
+++ LAMPAPI/deleteContact.php
@@ -0,0 +1,41 @@
+
+<?php
+	$inData = getRequestInfo();
+
+	$id = $inData["ID"];//The id of the contact to change
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ? AND UserID = ? "); 
+        $stmt->bind_param("ss", $id, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"
+                     "fields": "Id, userId"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ LAMPAPI/editContact.php
@@ -0,0 +1,46 @@
+
+<?php
+	$inData = getRequestInfo();
+	
+    $id = $inData["ID"];//The id of the contact to change
+	$userId = $inData["userId"];
+
+	$firstName = $inData["firstName"];
+    $lastName = $inData["lastName"]
+	$phone= $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE ID = ? AND UserID = ?");
+        $stmt->bind_param("ssssii", $firstName, $lastName, $phone, $email, $id, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"
+                     "fields": "ID, UserId, firstName, lastName, phone, email"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
