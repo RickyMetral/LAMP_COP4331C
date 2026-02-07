f03ab85ad3a3c1d1d013a1e9659b3d048c032f0e
Ricardo Metral <ricardometral2005@gmail.com>
2026-02-07 17:57:14 -0500

    Changed API Endpoint filenames to match

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-const urlBase = 'https://lampstackcolorslukevernon.site//LAMPAPI';
+const urlBase = 'https://groupsixlampproject.app/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
@@ -116,7 +116,7 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
-function addColor()
+function addContact()
 {
 	let newColor = document.getElementById("colorText").value;
 	document.getElementById("colorAddResult").innerHTML = "";
@@ -124,7 +124,7 @@ function addColor()
 	let tmp = {color:newColor,userId,userId};
 	let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/AddColor.' + extension;
+	let url = urlBase + '/AddContact.' + extension;
 	
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -147,7 +147,7 @@ function addColor()
 	
 }
 
-function searchColor()
+function searchContact()
 {
 	let srch = document.getElementById("searchText").value;
 	document.getElementById("colorSearchResult").innerHTML = "";
@@ -157,7 +157,7 @@ function searchColor()
 	let tmp = {search:srch,userId:userId};
 	let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/SearchColors.' + extension;
+	let url = urlBase + '/SearchContact.' + extension;
 	
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
